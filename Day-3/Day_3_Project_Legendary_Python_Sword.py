3fafc06a93dc4d8aad1f8521316339b30c514b00
captainx007 <shadsalman21156@gmail.com>
2025-07-09 23:01:22 +0600

    Create Day_3_Project_Legendary_Python_Sword.py

---
+++ Day-3/Day_3_Project_Legendary_Python_Sword.py
@@ -0,0 +1,84 @@
+print(r'''                                ,-.
+                               ("O_)
+                              / `-/
+                             /-. /
+                            /   )
+                           /   /  
+              _           /-. /
+             (_)"-._     /   )
+               "-._ "-'""( )/    
+                   "-/"-._" `. 
+                    /     "-.'._
+                   /\       /-._"-._
+    _,---...__    /  ) _,-"/    "-(_)
+___<__(|) _   ""-/  / /   /
+ '  `----' ""-.   \/ /   /
+               )  ] /   /
+       ____..-'   //   /                       )
+   ,-""      __.,'/   /   ___                 /,
+  /    ,--""/  / /   /,-""   """-.          ,'/
+ [    (    /  / /   /  ,.---,_   `._   _,-','
+  \    `-./  / /   /  /       `-._  """ ,-'
+   `-._  /  / /   /_,'            ""--"
+       "/  / /   /"         
+       /  / /   /
+      /  / /   /  o!O
+     /  |,'   /  
+    :   /    /
+    [  /   ,'   "Blackadder"
+    | /  ,'
+    |/,-'
+    P'
+    ''')
+print("Welcome to the 'Veil of Despair' the island of darkness!!💀")
+print("Your Mission is to find the LEGENDARY PYTHON SWORD!!")
+
+print("You are in middle of no where. Where do u wanna go?")
+go = input("     Type \"left\",\"right\" or \"straight\"? \n    ->>")
+
+if go == "left" or go == "l" or go == "L" or go == "Left":
+    print("You are doomed by a demon")
+elif go == "right" or go == "r" or go == "Right" or go == "R":
+    print("Suddenly a ghost came, and......")
+    b = input("Do u want to process? yes or no?")
+    if b == "yes":
+        print("processing..........")
+        a = input("Do you really wish to know what happen? press 'y' for Yes and 'n' for No")
+    print("You Died. lol 😂")
+else:
+    print("You saw the sword on top of the mountain full of ghost")
+    go = input("How do You want to go to top of that mountain? \n'sneak in' type a\n'keep walking🙄' type b\n-->")
+    if go == "a":
+        print("U are sneaking in front of ghosts? Are U sure? You died")
+    else:
+        print("That was bold but now u have to face the ghosts")
+        n =input("Run and try to grab the sword. press 'a'\nfight the ghost bravely. type 'b'\n->>")
+        if n == "a":
+            print("Sorry you are not faster than ghost so You died")
+        else:
+            print("Are you sure want to fight ghost bare handed? What a fool")
+print("Thanks for playing this lame game.")
+print("")
+play_again= input("Do U want to play again? yes or no?\n->>")
+if play_again == "yes" or play_again == "y":
+    print("You are in middle of no where. Where do u wanna go?")
+    go = input("     Type \"left\",\"right\",\"straight\"or \"up\"? \n    ->>")
+
+    if go == "left" or go == "l" or go == "L" or go == "Left":
+        print("You are doomed by a demon")
+    elif go == "right" or go == "r" or go == "Right" or go == "R":
+        print("Suddenly a ghost came, and......")
+        b = input("Do u want to process? yes or no?")
+        if b == "yes":
+            print("processing..........")
+            a = input("Do you really wish to know what happen? press 'y' for Yes and 'n' for No")
+        print("You Died. lol 😂")
+    elif go == "up" or go == "Up" or go =="u" or go == "U":
+        print("Well u can always fly u know...")
+        n = input("Keep flying and grab the sword? type 'a'\n Again fight the ghosts bravely? type 'b'\n->> ")
+        if n == "a":
+            print("Congratulation You have the Legendary Python Sword")
+        else:
+            print("Well You died again!!")
+    else:
+        print("Well you already no what happened last time. Don't you?😜")
