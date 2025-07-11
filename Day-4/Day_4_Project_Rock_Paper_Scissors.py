fd73e6dcef44517a2436a9c1c3dcae93d35e7550
captainx007 <shadsalman21156@gmail.com>
2025-07-11 11:24:11 +0600

    Update Day_4_Project_Rock_Paper_Scissors.py

--- Day-4/Day_4_Project_Rock_Paper_Scissors.py
+++ Day-4/Day_4_Project_Rock_Paper_Scissors.py
@@ -26,36 +26,29 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-#don't put float value then it will give value error. put int value only
-user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n->> ")) 
+#Don't put floating value. It will give ValueError
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n->> "))
 
-if user_input == 0:
-    print(rock)
-elif user_input == 1:
-    print(paper)
-elif user_input ==2:
-    print(scissors)
+moves = [rock, paper, scissors]
+computer_choose = random.randint(0,2)
+
+if user_input>2 or user_input<0 :
+    print("Invalid input. You lost.")
 else:
-    print("You have put wrong number. You lost!")
-
-set1 = [rock, paper, scissors]
-rand_number = random.randint(0,2)
-computer_choose = set1[rand_number]
-if user_input>=0 and user_input<=2:
-    print("Computer choose :- ")
-    print(computer_choose)
-
-    if user_input == rand_number:
-        print("Draw")
-    elif (user_input == 0) and (rand_number == 1):
-        print("Lost")
-    elif (user_input == 0) and (rand_number == 2):
-        print("Win")
-    elif (user_input == 1) and (rand_number == 0):
-        print("Win")
-    elif (user_input == 1) and (rand_number == 2):
-        print("Lost")
-    elif (user_input == 2) and (rand_number == 0):
-        print("Lost")
-    elif (user_input == 2) and (rand_number == 1):
-        print("Win")
+    print(moves[user_input])
+    print(f"Computer Choose: {moves[computer_choose]}")
+    if user_input == computer_choose:
+        print("It's a Draw")
+    elif user_input == 0 and computer_choose == 2:
+        print("You Win!")
+    elif user_input == 0 and computer_choose == 1:
+        print("You lose.")
+    elif user_input == 1 and computer_choose == 0:
+        print("You Win!")
+    elif user_input == 1 and computer_choose == 2:
+        print("You lose.")
+    elif user_input == 2 and computer_choose == 0:
+        print("You lose.")
+    elif user_input == 2 and computer_choose == 1:
+        print("You Win!")
+        
