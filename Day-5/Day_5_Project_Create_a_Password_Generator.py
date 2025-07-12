c7d90bb489082193781c9d9a8f7babd26244b20d
captainx007 <shadsalman21156@gmail.com>
2025-07-12 14:46:23 +0600

    Update Day_5_Project_Create_a_Password_Generator.py

--- Day-5/Day_5_Project_Create_a_Password_Generator.py
+++ Day-5/Day_5_Project_Create_a_Password_Generator.py
@@ -9,6 +9,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# my version
 random_letters = random.choices(letters,k = nr_letters)#here choose a random letter list
 letter = ""
 
@@ -32,3 +33,37 @@ password = letter+symbol+number
 
 random_password = ''.join(random.sample(password,len(password)))
 print(f"Your password is : {random_password}")
+#video version
+## Easy Version
+# password =""
+#
+# for char in range(0, nr_letters):
+#      password += random.choice(letters)
+#
+# for sym in range(0, nr_letters):
+#      password += random.choice(symbols)
+#
+# for num in range(0, nr_letters):
+#      password += random.choice(numbers)
+#
+# print(password)
+
+## Hard Version
+# password_list =[]
+# 
+# for char in range(0, nr_letters):
+#      password_list.append(random.choice(letters))
+# 
+# for sym in range(0, nr_letters):
+#      password_list.append(random.choice(symbols))
+# 
+# for num in range(0, nr_letters):
+#      password_list.append(random.choice(numbers))
+# 
+# print(password_list)
+# random.shuffle(password_list)
+# print(password_list)
+# password =""
+# for i in password_list:
+#      password += i
+# print(f"Your password is : {password}")
